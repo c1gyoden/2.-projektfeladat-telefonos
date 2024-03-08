d1269c9c88f3fc8a922b29d0868a9accd14fad10
c1gyoden <c1gyoden@vasvari.org>
2024-03-08 20:30:21 +0100

    2. feladat

--- telefon.py
+++ telefon.py
@@ -1,6 +1,24 @@
+import math
+
 def mpbe(o, p, mp):
-    mp = mp + 60*p + 3600*o
-    print(mp)
+    mp = int(mp) + 60*int(p) + 3600*int(o)
+    return mp
+def vissza(mp):
+    o = math.floor(mp/3600)
+    mp -= o*3600
+    p = math.floor(mp/60)
+    mp -= p*60
+    return o, p, mp
 
-mpbe(1,5,10)
+f = open('hivas.txt', 'rt')
 
+hivasok = []
+for sor in f:
+    sor = sor.strip().split()
+    print(sor)
+    tmp = []
+    tmp.append(mpbe(sor[0], sor[1], sor[2]))
+    tmp.append(mpbe(sor[3], sor[4], sor[5]))
+    hivasok.append(tmp)
+
+print(hivasok)
