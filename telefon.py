e43d3fec8d914a82ba83b57e6aba3c783a47af06
c1gyoden <c1gyoden@vasvari.org>
2024-03-08 20:59:42 +0100

    újradolgozás classba

--- telefon.py
+++ telefon.py
@@ -1,14 +1,13 @@
 import math
+class Szamolas():
+    def __init__(self, o, p, mp):
+        self.o = o
+        self.p = p
+        self.mp = mp
 
-def mpbe(o, p, mp):
-    mp = int(mp) + 60*int(p) + 3600*int(o)
-    return mp
-def vissza(mp):
-    o = math.floor(mp/3600)
-    mp -= o*3600
-    p = math.floor(mp/60)
-    mp -= p*60
-    return o, p, mp
+    def mpbe(self):
+        mp = int(self.mp) + 60*int(self.p) + 3600*int(self.o)
+        return mp
 
 f = open('hivas.txt', 'rt')
 
@@ -17,8 +16,9 @@ for sor in f:
     sor = sor.strip().split()
     print(sor)
     tmp = []
-    tmp.append(mpbe(sor[0], sor[1], sor[2]))
-    tmp.append(mpbe(sor[3], sor[4], sor[5]))
+    tmp.append(Szamolas(sor[0], sor[1], sor[2]))
+    tmp.append(Szamolas(sor[3], sor[4], sor[5]))
     hivasok.append(tmp)
 
-print(hivasok)
+
+
